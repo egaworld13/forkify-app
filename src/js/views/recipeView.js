941a3726785746c2e8a5bb82f158cf4483a6f853
Edgars Roze <edgarsroze13@gmail.com>
2022-12-16 13:05:38 +0200

    fraction fixing

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
 //? fraction is for nicely render decimal of recipe ingridients qauntity
-
+import fracty from 'fracty';
 //? Class with private methods and variables
 class RecipeView extends View {
   //? Private fields
@@ -128,7 +128,7 @@ class RecipeView extends View {
        <use href="${icons}#icon-check"></use>
     </svg>
      <div class="recipe__quantity">${
-       ing.quantity ? new Fraction(ing.quantity).toString() : ''
+       ing.quantity ? fracty(ing.quantity).toString() : ''
      }</div>
      <div class="recipe__description">
        <span class="recipe__unit">${ing.unit}</span>
